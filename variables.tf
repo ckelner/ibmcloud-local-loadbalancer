1036de3315c728a18eebfe5c3befdbd25085cba8
ckelner <chris.kelner@gmail.com>
2017-03-19 11:22:47 -0400

    stub vars

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "connections" {
+  description = ""
+}
+
+variable "datacenter" {
+  description = ""
+}
+
+variable "ha_enabled" {
+  description = ""
+}
+
+variable "dedicated" {
+  description = ""
+}
+
+variable "service_group_port" {
+  description = ""
+}
+
+variable "service_group_routing_method" {
+  description = ""
+}
+
+variable "service_group_routing_type" {
+  description = ""
+}
+
+variable "service_group_allocation" {
+  description = ""
+}
