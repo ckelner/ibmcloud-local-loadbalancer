17ae26aa439eb18c4698267658b3c9f50b151dae
ckelner <chris.kelner@gmail.com>
2017-03-19 13:05:31 -0400

    fix output for lb addr

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "loadbalancer_id" {
     value = "${ibmcloud_infra_lb_local.lb.id}"
 }
 output "loadbalancer_address" {
-    value = "http://${ibmcloud_infra_lb_local.lb.ip_address}:${var.port}/"
+    value = "http://${ibmcloud_infra_lb_local.lb.ip_address}:${var.service_group_port}/"
 }
 output "service_group_id" {
     value = "${ibmcloud_infra_lb_local_service_group.lb_service_group.service_group_id}"
