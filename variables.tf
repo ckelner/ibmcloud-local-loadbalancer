81ff19a7f1c830d079efde38af8708c88cdfad7b
ckelner <chris.kelner@gmail.com>
2017-03-19 12:43:28 -0400

    Add defaults

--- variables.tf
+++ variables.tf
@@ -4,29 +4,35 @@ variable "connections" {
 }
 
 variable "datacenter" {
-  description = "The IBM Cloud datacenter to create the load balancer in"
+  description = "The IBM Cloud datacenter to create the load balancer in. README contains datacenters ids"
 }
 
 variable "ha_enabled" {
   description = "(Boolean) Determines if the LB is highly available or not"
+  default = false
 }
 
 variable "dedicated" {
   description = "(Boolean) Whether the LB is dedicated (single tenant) or not"
+  default = false
 }
 
 variable "service_group_port" {
   description = "The port that the load balancer will serve traffic on"
+  default = 80
 }
 
 variable "service_group_routing_method" {
   description = "Routing method for the load balancer, valid options are: CONSISTENT_HASH_IP, INSERT_COOKIE, LEAST_CONNECTIONS, LEAST_CONNECTIONS_INSERT_COOKIE, LEAST_CONNECTIONS_PERSISTENT_IP, PERSISTENT_IP, ROUND_ROBIN, ROUND_ROBIN_INSERT_COOKIE, ROUND_ROBIN_PERSISTENT_IP, SHORTEST_RESPONSE, SHORTEST_RESPONSE_INSERT_COOKIE, SHORTEST_RESPONSE_PERSISTENT_IP. See http://knowledgelayer.softlayer.com/procedure/what-balancing-methods-are-available-load-balancer for more information."
+  default = "ROUND_ROBIN"
 }
 
 variable "service_group_routing_type" {
   description = "The communication protocol the load balancer will route traffic using. Options are: HTTP, TCP, DNS, FTP, UDP, and HTTPS"
+  default = "HTTP"
 }
 
 variable "service_group_allocation" {
   description = "The percentage of connections to allocate to the group"
+  default = 100
 }
